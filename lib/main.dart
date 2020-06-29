b680b13bf5e071dd99406e77c76939de10252857
Hammad Khan <hammadkhn30@gmail.com>
2020-06-29 21:31:36 +0530

    added the routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: "RestaurantFinder",
+      theme: ThemeData(
+        primaryColor: Colors.orangeAccent,
+        accentColor: Colors.yellowAccent,
+        canvasColor: Color.fromRGBO(255,254,229,1),
+      ),
+      initialRoute: '/',
+      routes: {
+        
+      },
+    );
   }
 }
 
