af8f3a30d3100d19e4beadd48f2b92af750439d6
Hammad Khan <hammadkhn30@gmail.com>
2020-07-03 23:52:11 +0530

    added the search box

--- lib/UI/location_screen.dart
+++ lib/UI/location_screen.dart
@@ -5,8 +5,29 @@ class LocationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("Restaurant Quaerere"),
-        //color: Color
+        title: Text("Restaurant Quaerere"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(11),
+            child: TextField(
+              autocorrect: true,
+              decoration: InputDecoration(
+                hintText: 'Select The city',
+                hintStyle: TextStyle(color: Theme.of(context).primaryColor),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.elliptical(12, 6),
+                  ),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
