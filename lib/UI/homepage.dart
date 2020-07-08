5c071fc59e147dfca3000e28a9f5c63a52c26d3c
Hammad Khan <hammadkhn30@gmail.com>
2020-07-08 23:24:48 +0530

    wired location screen

--- lib/UI/homepage.dart
+++ lib/UI/homepage.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_finder/Bloc/bloc_provider.dart';
+import 'package:restaurant_finder/Bloc/location_bloc.dart';
+import 'package:restaurant_finder/DataLayer/location.dart';
 import './location_screen.dart';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LocationScreen();
+    return StreamBuilder<Location>(
+      // 1
+      stream: BlocProvider.of<LocationBloc>(context).locationStream,
+      builder: (context, snapshot) {
+        final location = snapshot.data;
+
+        // 2
+        if (location == null) {
+          return LocationScreen();
+        }
+
+        // This will be changed this later
+        return Container();
+      },
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_finder/Bloc/location_bloc.dart';
 import './UI/homepage.dart';
+import './Bloc/bloc_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "RestaurantFinder",
-      theme: ThemeData(
-        primaryColor: Colors.teal[700],
-        accentColor: Colors.tealAccent[700],
-        canvasColor: Color.fromRGBO(255,254,229,1),
+    return BlocProvider<LocationBloc>(
+      bloc: LocationBloc(),
+      child: MaterialApp(
+        title: "RestaurantFinder",
+        theme: ThemeData(
+          primaryColor: Colors.teal[700],
+          accentColor: Colors.tealAccent[700],
+          canvasColor: Color.fromRGBO(255,254,229,1),
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) => Homepage(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => Homepage(),
-      },
     );
   }
 }
